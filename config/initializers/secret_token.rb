c1e31d58f45dc40197a10eed80972d9c9e723796
sharilyn <sharilyn.neidhardt@gmail.com>
2014-01-02 03:41:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tenthousandhours::Application.config.secret_key_base = '29bca8905a81cc4d906623ed9448f50100e10bc27dbe0da628dfbe0674cb45713f6e9369d8d16366f7369c43dfc298628d989261313b029152c88c9de0817597'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tenthousandhours::Application.config.session_store :cookie_store, key: '_tenthousandhours_session'
