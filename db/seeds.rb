3b1940f31d4f87d62ea2be321e2b06f233857079
sharilyn <sharilyn.neidhardt@gmail.com>
2014-01-02 13:08:55 -0500

    added skills resource and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Skill.create(name: "Chess", description:"Ancient boardgame using strategy")
+Skill.create(name: "Yoga", description:"Meditation through guided movement")
+Skill.create(name: "Ruby on Rails", description:"Popular programming framework")
+Skill.create(name: "Painting", description:"Image creation using ligments applied to flat surfaces")
