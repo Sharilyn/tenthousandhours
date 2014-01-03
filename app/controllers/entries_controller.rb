db2a83268e2075353be2b81b224d57d47823e983
sharilyn <sharilyn.neidhardt@gmail.com>
2014-01-03 08:51:55 -0500

    new entry form working - thanks Steven

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,31 +1,38 @@
 class EntriesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
 
-    def index
-        @entries = Entry.all
-    end
+  def index
+    @entries = Entry.all
+  end
 
-    def show
-        @entry = Entry.find params[:id]
-    end
+  def show
+    @entry = Entry.find params[:id]
+  end
 
-    def new
-        @entry = Entry.new
-        @users = User.all
-        @skills = Skill.all
-    end
+  def new
+    @entry = Entry.new
+    @skills = Skill.all
+  end
 
-    def create
-        @entry = Entry.new safe_entry
-        if @entry.save
-            flash[:notice] = "You have added a new entry to your skillset"
-            redirect_to @entry
-        else
-            render 'new'
+  def update
+       # Find the Entry by id
+       # Safe_params stuff
+       # update entry
+       #redirect to entry
+     end
+
+  def create
+    @entry = current_user.entries.new safe_entry
+    if @entry.save
+      flash[:notice] = "You have added a new entry to your skillset"
+      redirect_to @entry
+    else
+      render 'new'
     end
-end
-    private
+  end
+
+  private
     def safe_entry
-        safe_entry = params.require(:entry).permit(:hours, :note, :user_id, :skill_id)
+      params.require(:entry).permit(:hours, :note, :user_id, :skill_id)
     end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
   belongs_to :skill
+
+ 
+
 end
