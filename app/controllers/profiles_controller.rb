f685358ea12d7029e3a2fcfcf412504d4b7802f7
sharilyn <sharilyn.neidhardt@gmail.com>
2014-01-08 21:59:05 -0500

    still cant get sum to work

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,8 @@ class ProfilesController < ApplicationController
         @users = User.all
         @skills = Skill.all
         @user = User.find params[:id]
+        @skill = Skill.find params[:id]
+        @entries = Entry.all
     end
 
     def index
